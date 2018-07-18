e09a49d2c494934dc116ab1e6ba7312fe78e2a2d
Victor Gómez <victormgomez13@gmail.com>
2018-07-18 11:25:26 -0500

    last-change

--- db.php
+++ db.php
@@ -1,60 +1,45 @@
 <?php
-    class Data_Base {
-        //Class conection
-        private $db;
-        private $msg;
-        private $status = false;
-        private $data = array();
-        private function db_open(){
-            $this->db = new mysqli('localhost','homestead','secret','data');
-            if($this->db->connect_errno)
-                $this->msg = "Ocurrio un Problema En la base de datos";
-            else{
-                $this->db->set_charset('utf8');
-                $this->msg = "Conexion Exitosa";
-                $this->status = true;
-            }
-                
+    
+class Data_Base{
+    private $status = false;
+    private $db ;
+    
+    private function db_open(){
+        $this->db = new mysqli('localhost','homestead','secret','data');
+        if(!$this->db->connect_errno){
+            $this->status = true;
+            $this->db->set_charset('utf8');
         }
-        
-        private function db_close(){
-            $this->db->close();
-        }
-        
-        public  function set_data($sql){
-            $this->db_open();
-            if( $this->status ){
-                if( !$this->db->query($sql) ){
-                    $this->status = false;
-                    $this->msg = 'Algo salió mal al insertar los datos';
-                }
-                else{
-                    $msg = 'Datos Agregados';
-                    $status = true;
-                }
-                
-            }
-            $this->data = array('msg' => $msg, 'status' => $status);
-            $this->db_close();
-            return $this->data;
-        }
-        
-        public function get_data($sql){
-            $rows = array();
-            $this->db_open();
-            if($this->status){
-                $result = $this->db->query($sql);
-                if($result->num_rows > 0 ){
-                    while($rows[] = $result->fetch_assoc());
-                     $this->db_close();
-                    
-                }
-                else{
-                    $rows = false;
-                }
-                
-             }
-                return $rows;
-            }
         
     }
+    
+    public function db_close(){
+        $this->db->close();
+    }
+    
+    public function set_data($sql){
+        $this->db_open();
+        if($this->status){
+            if(!$this->db->query($sql))
+                $this->status = false;
+            $this->db_close();
+        }
+        return $this->status;
+    }
+    
+    public function get_data($sql){
+        $this->db_open();
+        $row = array();
+        if($this->status){
+            $result = $this->db->query($sql);
+            if($result->num_rows >0){
+                while($row[] = $result->fetch_assoc());
+                $this->db->close();
+                array_pop($row);
+            }
+            
+        }
+        return $row;
+    }
+    
+}
